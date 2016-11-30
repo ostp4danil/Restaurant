093175511e3dedff078a73cc61826e782045ff4f
danil <ostapchenko.danil@gmail.com>
2016-11-30 02:57:42 +0200

    fixik

--- src/main/java/daoandmodels/dish/IDishDAO.java
+++ src/main/java/daoandmodels/dish/IDishDAO.java
@@ -1,32 +1,30 @@
 package daoandmodels.dish;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  * Created by Danil-MAC on 11/28/16.
  */
+
 public class IDishDAO implements DishDAO {
-    @Override
+
     public List<Dish> getAll() {
         return null;
     }
 
-    @Override
     public Dish get(Integer id) {
         return null;
     }
 
-    @Override
     public void add(Dish dish) {
 
     }
 
-    @Override
     public void delete(Integer id) {
 
     }
 
-    @Override
     public void edit(Dish dish) {
 
     }
